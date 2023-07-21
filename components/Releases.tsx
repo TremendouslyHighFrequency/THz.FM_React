4dca9071eb5e65333c6e8dfdaed08b0ebb1f3cb3
nn-dmt <nn@dmt.pub>
2023-07-21 10:44:56 -0700

    fix track & release slug generation

--- components/Releases.tsx
+++ components/Releases.tsx
@@ -30,7 +30,7 @@ const Releases = () => {
                           <div className="album-text">
                                   <h4>{title}</h4>
                                   <p>{release_artist}</p>
-                                  <Link to={`/releases/${title}`}>View Release</Link>
+                                  <Link to={`/releases/${release_artist}/${title}`}>View Release</Link>
                               </div>
                           </div>
   
