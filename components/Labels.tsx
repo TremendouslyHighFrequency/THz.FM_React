e651bc7d6d4055528853b2cb1a8bf6e91b0c197a
nn-dmt <nn@dmt.pub>
2023-07-23 12:55:46 -0700

    collection item edits

--- components/Labels.tsx
+++ components/Labels.tsx
@@ -1,13 +1,45 @@
-// Labels.jsx
-import React from 'react';
+import React, { useState } from 'react';
+import { useFrappeGetDocList } from 'frappe-react-sdk';
+import { LabelItem } from '../types';
+import { Link } from "react-router-dom";
 
 const Labels = () => {
-  return (
-    <div className="content">
-      This is the Labels page.
-    </div>
-  );
+  const [pageIndex, setPageIndex] = useState<number>(0)
+  const { data, error, isValidating } = useFrappeGetDocList<LabelItem>('Label' , {
+      fields: ["title", "label_bio","label_photo"],
+      limit_start: pageIndex,
+      limit: 50,
+      orderBy: {
+          field: "creation",
+          order: 'desc'
+      }
+  });
+
+  if (isValidating) {
+      return <>Loading</>
+  }
+  if (error) {
+      return <>{JSON.stringify(error)}</>
+  }
+  if (data && Array.isArray(data)) {
+         return (
+          <div className="albums-index">
+                  {
+                      data.map(({title, label_photo, label_bio}, i) => (
+                          <div key={i} className="album-card" style={{backgroundImage: `url(${label_photo})`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
+                          <div className="album-text">
+                                  <h4>{title}</h4>
+                                  <Link to={`/label/${title}`}>View Label</Link>
+                              </div>
+                          </div>
+  
+                      ))
+                  }
+                  <button onClick={() => setPageIndex(pageIndex + 50)}>Next page</button>
+          </div>
+      )
+  }
+  return null
 };
 
 export default Labels;
-
