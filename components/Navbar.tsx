7933221c669fadc04076d3a4616f835097fbb770
nn-dmt <nn@dmt.pub>
2023-07-23 20:20:21 -0700

    dappcolor

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -40,7 +40,7 @@ const Navbar = ({ loggedUser, notifications }: NavbarProps & { notifications: No
             onBlur={() => setIsExpanded(false)}
           />
           <div className="dapp-button">
-            <ErgoDappConnector color="cyan" />
+            <ErgoDappConnector color="inkwell" />
           </div>
           
           <button className="bell" ref={notificationButtonRef} onClick={() => setDropdownVisible(prev => !prev)}>
