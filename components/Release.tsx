72dc07f9651c9a29c44d5b7f4ef701eaa8b1a28d
nn-dmt <nn@dmt.pub>
2023-07-24 17:21:29 -0700

    release page edits

--- components/Release.tsx
+++ components/Release.tsx
@@ -8,13 +8,6 @@ const Release = () => {
   const { title, release_artist } = useParams();
   const { data, error, isValidating } = useFrappeGetDoc<ReleaseItem>('Release', title); // assuming 'title' can be used to fetch a single ReleaseItem
 
-  const WaveSurfer = WaveSurfer.create({
-    container: '#waveform',
-    waveColor: '#4F4A85',
-    progressColor: '#383351',
-    url: 'https://thz.fm{(track.attach_mp3}}',
-  });
-
   useEffect(() => {
     // do something when title or artist changes, such as fetch related data
   }, ["title", "release_id", "release_artwork", "release_artist", "release_label", "release_description", "release_tracks[]", "release_credits", "release_date"]);
@@ -27,7 +20,14 @@ const Release = () => {
     return <>{JSON.stringify(error)}</>
   }
 
-  if (data) {   
+  if (data) {  
+    const WaveSurfer = WaveSurfer.create({
+      container: '#waveform',
+      waveColor: '#4F4A85',
+      progressColor: '#383351',
+      url: 'https://thz.fm{(track.attach_mp3}}',
+    })
+     
     return (
       <div>
         {/* Display the data */}
