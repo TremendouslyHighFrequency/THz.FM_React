18003aa641305ba4206b822786727ba5cb203ad6
nn-dmt <nn@dmt.pub>
2023-07-25 16:58:14 -0700

    change from react router to frappe router

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 //React Imports
-import React, { useState, useRef, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 
 //Frappe Imports 
 import { FrappeProvider } from 'frappe-react-sdk';
@@ -29,10 +28,19 @@ import Single from './components/Single';
 
 function App() {
 
+  const [route, setRoute] = useState([]);
   const [notifications, setNotifications] = useState<Notification[]>([]);
   const notificationButtonRef = useRef<HTMLButtonElement | null>(null);
 
   useEffect(() => {
+    // Initialize route
+    setRoute(frappe.get_route());
+
+    // Listen for route changes
+    window.onpopstate = () => {
+      setRoute(frappe.get_route());
+    };
+
     getLoggedUser()
       .then(loggedUser => getNotifications(loggedUser))
       .then(notificationData => setNotifications(notificationData))
@@ -47,65 +55,80 @@ function App() {
     }
   };
 
-async function purchase() {
-  // requests wallet access
-  if (await ergoConnector.nautilus.connect()) {
-    // get the current height from the the dApp Connector
-    const height = await ergo.get_current_height();
+  async function purchase() {
+    // requests wallet access
+    if (await ergoConnector.nautilus.connect()) {
+      // get the current height from the the dApp Connector
+      const height = await ergo.get_current_height();
 
-    const unsignedTx = new TransactionBuilder(height)
-      .from(await ergo.get_utxos()) // add inputs from dApp Connector
-      .to(
-        // Add output
-        new OutputBuilder(
-          "100000000",
-          "9fjTtRPuaSXU3QuK73EH7w6dCd2Z8oPDnXz5qBptKpD6MUdwiZX"
+      const unsignedTx = new TransactionBuilder(height)
+        .from(await ergo.get_utxos()) // add inputs from dApp Connector
+        .to(
+          // Add output
+          new OutputBuilder(
+            "100000000",
+            "9fjTtRPuaSXU3QuK73EH7w6dCd2Z8oPDnXz5qBptKpD6MUdwiZX"
+          )
         )
-      )
-      .sendChangeTo(await ergo.get_change_address()) // Set the change address to the user's default change address
-      .payMinFee() // set minimal transaction fee
-      .build() // build the transaction
-      .toEIP12Object(); // converts the ErgoUnsignedTransaction instance to an dApp Connector compatible plain object
+        .sendChangeTo(await ergo.get_change_address()) // Set the change address to the user's default change address
+        .payMinFee() // set minimal transaction fee
+        .build() // build the transaction
+        .toEIP12Object(); // converts the ErgoUnsignedTransaction instance to an dApp Connector compatible plain object
 
-    // requests the signature
-    const signedTx = await ergo.sign_tx(unsignedTx);
+      // requests the signature
+      const signedTx = await ergo.sign_tx(unsignedTx);
 
-    // send the signed transaction to the mempool
-    const txId = await ergo.submit_tx(signedTx);
+      // send the signed transaction to the mempool
+      const txId = await ergo.submit_tx(signedTx);
 
-    // prints the Transaction ID of the submitted transaction on the console
-    console.log(txId);
-  }
-};
+      // prints the Transaction ID of the submitted transaction on the console
+      console.log(txId);
+    }
+  };
 
   const { navItems, links } = SideNav();
 
+  // Render the right component based on the current route
+  let Component;
+  switch (route[0]) {
+    case 'Release':
+      Component = Release;
+      break;
+    case 'Product':
+      Component = Product;
+      break;
+    case 'Artist':
+      Component = Artist;
+      break;
+    case 'Event':
+      Component = Event;
+      break;
+    case 'Venue':
+      Component = Venue;
+      break;
+    case 'Single':
+      Component = Single;
+      break;
+    default:
+      Component = null;  // Default component if no route matches
+      break;
+  }
+
   return (
-    <Router>
-    <div className="App">
-      <FrappeProvider url='https://thz.fm'>
+    <FrappeProvider url='https://thz.fm'>
+      <div className="App">
         <div className="App-header" style={{ minHeight: '72px' }}>
           <Navbar loggedUser={null} notifications={notifications} />
         </div>
-        <div>
-        
- <div className="App-body">
-           <div className="main-container">
-           {links}
-           </div>
-            <div className="page-content">
-            
-            <Routes>
-  {navItems.map(item => (
-    <Route key={item.route} path={item.route} element={React.createElement(item.component)} />
-  ))}
-    <Route path="/releases/:title/by/:artist" element={<Release />} />
-</Routes>
-              <div id="comment-container"></div>
-            </div>
-         </div>
+        <div className="App-body">
+          <div className="main-container">
+            {links}
+          </div>
+          <div className="page-content">
+            {Component ? <Component /> : <div>No Component for this route</div>}
+            <div id="comment-container"></div>
+          </div>
         </div>
-    
         <div className="App-footer">
           <div className="footer">
             <div>
@@ -115,9 +138,8 @@ async function purchase() {
             </div>
           </div>
         </div>
-      </FrappeProvider>
-    </div>
-    </Router>
+      </div>
+    </FrappeProvider>
   );
 }
 
