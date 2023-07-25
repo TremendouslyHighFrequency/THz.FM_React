3d6e126066de1b3be3b7388f793673e99da639ac
nn-dmt <nn@dmt.pub>
2023-07-24 17:41:56 -0700

    release page edits

--- components/Release.tsx
+++ components/Release.tsx
@@ -1,54 +1,68 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import { useFrappeGetDoc } from 'frappe-react-sdk'; // assuming this hook exists
 import { ReleaseItem } from '../types';
 import WaveSurfer from 'wavesurfer.js';
 
-const Release = () => {
-  const { title, artist } = useParams();
-  const { data, error, isValidating } = useFrappeGetDoc<ReleaseItem>('Release', title); // assuming 'title' can be used to fetch a single ReleaseItem
-
+const Track = ({ track, index }) => {
+  const waveformRef = useRef(null);
+  
   useEffect(() => {
-    // do something when title or artist changes, such as fetch related data
-  }, ["title", "release_id", "release_artwork", "release_artist", "release_label", "release_description", "release_tracks[]", "release_credits", "release_date"]);
+    const wavesurfer = WaveSurfer.create({
+      container: waveformRef.current,
+      waveColor: '#4F4A85',
+      progressColor: '#383351',
+    });
+    wavesurfer.load(`https://thz.fm${track.attach_mp3}`);
+    
+    return () => {
+      wavesurfer.destroy();
+    };
+  }, [track]);
+
+  return (
+    <div key={index}>
+      <p>{track.title}</p>
+      <p>{track.artist}</p>
+      <div id={`waveform-${index}`} ref={waveformRef}></div>
+      <audio id={`audio-${index}`} crossOrigin src={`https://thz.fm${track.attach_mp3}`} type="audio/mpeg"></audio>
+    </div>
+  );
+}
+
+const Release = () => {
+  const { title } = useParams();
+  const { data, error, isValidating } = useFrappeGetDoc<ReleaseItem>('Release', title);
 
   if (isValidating) {
     return <>Loading...</>
   }
+
   if (error) {
     return <>{JSON.stringify(error)}</>
   }
+
   if (data) {
     return (
       <div>
         {/* Display the data */}
         <div className="album-page" style={{backgroundImage: `url(${data.release_artwork})`}}>
-        <div className="album-page-text">
-        <h1>{data.title}</h1>
-        <p>{data.release_artist}</p>
-        <p>{data.release_date}</p>
-        <p>{data.release_label}</p>
-        <p>{data.release_description}</p>
-        {data.release_tracks.map((track, index) => (
-          <div key={index}>
-            <p>{track.title}</p>
-            <p>{track.artist}</p>
-           <audio id="audio-{ loop.index }" crossorigin src="https://thz.fm{track.attach_mp3}" type="audio/mpeg"></audio>
+          <div className="album-page-text">
+            <h1>{data.title}</h1>
+            <p>{data.release_artist}</p>
+            <p>{data.release_date}</p>
+            <p>{data.release_label}</p>
+            <p>{data.release_description}</p>
+            {data.release_tracks.map((track, index) => (
+              <Track track={track} index={index} />
+            ))}
+            <p>{data.release_credits}</p>
           </div>
-        )
-        const wavesurfer = WaveSurfer.create({
-          container: '#waveform',
-          waveColor: '#4F4A85',
-          progressColor: '#383351',
-          url: 'https://thz.fm{track.attach_mp3}',
-        })
-        )}
-        <p>{data.release_credits}</p>
-        </div>
         </div>
       </div>
     )
   }
+
   return null;
 };
 
