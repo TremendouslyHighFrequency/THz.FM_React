f87f18980188ce656aa03c3046cc7ef209fdc55e
nn-dmt <nn@dmt.pub>
2023-07-27 18:44:57 -0700

    release page edits

--- components/Release.tsx
+++ components/Release.tsx
@@ -116,7 +116,7 @@ const FooterPlayer = ({ track, currentTime, duration }) => {
 
   return (
     <div className="footer-player">
-      <img src={`https://thz.fm${track.album_artwork}`} alt={track.track_title} />
+      <img src={`https://thz.fm/${track.album_artwork}`} alt={track.track_title} />
       <div className="track-details">
         <h2>{track.track_title}</h2>
         <p>{track.track_artist}</p>
