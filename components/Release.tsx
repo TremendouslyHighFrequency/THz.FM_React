bd56fd91c44abcb638ec4659da8e489009ba3880
nn-dmt <nn@dmt.pub>
2023-07-27 17:24:24 -0700

    release page edits

--- components/Release.tsx
+++ components/Release.tsx
@@ -50,21 +50,27 @@ const Track = ({ track, index, containerColor, waveformColor, releasetextColor, 
       cursorColor: 'rgba(0,0,0,0)',
       height: 50,
     });
-
+  
     wavesurferRef.current.on('audioprocess', function() {
       var currentTime = wavesurferRef.current.getCurrentTime();
       var duration = wavesurferRef.current.getDuration();
       updateTimer(currentTime, duration);
     });
-
+  
     wavesurferRef.current.on('finish', function() {
       onNext();
     });
-
+  
+    if (!playing) {
+      wavesurferRef.current.pause();
+    } else {
+      wavesurferRef.current.play();
+    }
+  
     return () => {
       wavesurferRef.current && wavesurferRef.current.destroy();
     };
-  }, [index, onNext]);
+  }, [index, playing, onNext]);
 
   useEffect(() => {
     wavesurferRef.current.load(`https://thz.fm${track.attach_mp3}`)
