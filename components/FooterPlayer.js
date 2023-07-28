9de748d39bca83ddbb514b138995200b8e710d21
nn-dmt <nn@dmt.pub>
2023-07-27 18:29:49 -0700

    release page edits

--- components/FooterPlayer.js
+++ components/FooterPlayer.js
@@ -2,18 +2,23 @@ import React from 'react';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 
 const FooterPlayer = ({ track, currentTime, duration }) => {
-  const percentage = (currentTime / duration) * 100;
-
-  return (
-    <div className="footer-player">
-      <img src={`https://thz.fm${track.album_artwork}`} alt={track.track_title} />
-      <div className="track-details">
-        <h2>{track.track_title}</h2>
-        <p>{track.track_artist}</p>
+    const percentage = (currentTime / duration) * 100;
+  
+    // Check if track is not null before trying to access its properties
+    if (!track) {
+      return null;
+    }
+  
+    return (
+      <div className="footer-player">
+        <img src={`https://thz.fm${track.album_artwork}`} alt={track.track_title} />
+        <div className="track-details">
+          <h2>{track.track_title}</h2>
+          <p>{track.track_artist}</p>
+        </div>
+        <ProgressBar now={percentage} />
       </div>
-      <ProgressBar now={percentage} />
-    </div>
-  );
-};
+    );
+  };
 
 export default FooterPlayer;
