551f5b94dd25541b02456b42a4c98ca61862917c
nn-dmt <nn@dmt.pub>
2023-07-29 22:05:21 -0700

    release page edits

--- components/PaymentMonitorContext.js
+++ components/PaymentMonitorContext.js
@@ -1,43 +1,9 @@
-// PaymentMonitorContext.js
-import React, { createContext, useState, useEffect } from 'react';
-import axios from 'axios';
+import { createContext, useContext } from 'react';
 
 const PaymentMonitorContext = createContext({
-  transactionConfirmed: false,
-  setTransactionConfirmed: () => {},
   txId: null,
   setTxId: () => {},
 });
-
-export function PaymentMonitorProvider({ children }) {
-  const [transactionConfirmed, setTransactionConfirmed] = useState(false);
-  const [txId, setTxId] = useState(null);
-
-  useEffect(() => {
-    if (txId) {
-      const interval = setInterval(async () => {
-        try {
-          const response = await axios.get('https://api.ergoplatform.com/api/v1/transactions/' + txId);
-          if (response.status === 200 && response.data) {
-            setTransactionConfirmed(true);
-            clearInterval(interval);
-          }
-        } catch (error) {
-          if (error.response && error.response.status === 404) {
-            setTransactionConfirmed(false);
-          }
-        }
-      }, 20000);
-      return () => clearInterval(interval);
-    }
-  }, [txId]);
-
-  return (
-    <PaymentMonitorContext.Provider value={{ transactionConfirmed, setTransactionConfirmed, txId, setTxId }}>
-      {children}
-    </PaymentMonitorContext.Provider>
-  );
-}
 
 export function usePaymentMonitor() {
   return useContext(PaymentMonitorContext);
