e296ee15afd61816791a3d59477a686463fc9584
nn-dmt <nn@dmt.pub>
2023-07-31 09:49:45 -0700

    search

--- components/Tracks.tsx
+++ components/Tracks.tsx
@@ -4,13 +4,26 @@ import { Link } from "react-router-dom";
 
 const Tracks = () => {
   const [tracks, setTracks] = useState([]);
+  const [parents, setParents] = useState({}); // new state variable to store parent releases
   const [pageIndex, setPageIndex] = useState(0); // initial value
 
   useEffect(() => {
     const fetchTracks = async () => {
       try {
         const trackResponse = await axios.get(`https://thz.fm/api/resource/Track?fields=["track_title","track_number","parent","track_artist","remixer","track_type","attach_wav","attach_mp3","price_usd","price_erg","label","artwork","route","published"]&parent=Release&limit_page_length=10&limit_start=${pageIndex}`);
-        setTracks(trackResponse.data.data);
+        const trackData = trackResponse.data.data;
+
+        const parentNames = trackData.map(track => track.parent);
+        const uniqueParentNames = [...new Set(parentNames)];
+        const parentPromises = uniqueParentNames.map(name => axios.get(`https://thz.fm/api/resource/Release/${name}`));
+        const parentResponses = await Promise.all(parentPromises);
+        const parentData = parentResponses.reduce((obj, response, i) => {
+          obj[uniqueParentNames[i]] = response.data.data.title;
+          return obj;
+        }, {});
+
+        setTracks(trackData);
+        setParents(parentData);
       } catch (error) {
         console.error(`Error fetching data: ${error}`);
       }
@@ -30,7 +43,7 @@ const Tracks = () => {
           <div className="album-text">
             <h4>{track.track_title}</h4>
             <p>{track.track_artist}</p>
-            <Link to={`/releases/${track.parent.title}/${track.track_title}/by/${track.track_artist}`}>View Track</Link>
+            <Link to={`/releases/${parents[track.parent]}/${track.track_title}/by/${track.track_artist}`}>View Track</Link>
           </div>
         </div>
       ))}
