97f6f58afb8e1df612bd4faab8fa7e14c22b3ae6
nn-dmt <nn@dmt.pub>
2023-07-31 10:18:22 -0700

    search

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -105,28 +105,28 @@ const Navbar = ({ loggedUser, notifications, setTxId, txId }: NavbarProps & { no
         <button onClick={toggleTheme}>
         {theme === 'dark' ? <SunIcon size={24} /> : <MoonIcon size={24} />}
       </button>
-      <div className="search-container">
-  <input
-    className="navbar-search"
-    type="text"
-    placeholder="Search..."
-    value={search}
-    onChange={(e) => setSearch(e.target.value)}
-    onClick={() => setIsExpanded(true)}
-    onBlur={() => setIsExpanded(false)}
-  />
-  <div className="search-results">
-    {searchResults.length > 0 && (
-      <div className="navbar-dropdown show">
-        {searchResults.map((result, index) => (
+          <input
+            className={`navbar-search ${isExpanded ? 'full-width' : ''}`}
+            type="text"
+            placeholder="Search..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            onClick={() => setIsExpanded(true)}
+            onBlur={() => setIsExpanded(false)}
+          />
+          <div className="search-results">
+          {
+          searchResults.length > 0 && (
+          <div className="navbar-dropdown show">
+          {searchResults.map((result, index) => (
           <div key={index} className="navbar-dropdown-item">
-            <p>{result.title}</p>
-          </div>
-        ))}
+           <p>{ result.title }</p>
+           </div>
+             ))}
+            </div>
+          )
+         }
       </div>
-    )}
-  </div>
-</div>
           <div className="dapp-button">
             <ErgoDappConnector color="inkwell" />
           </div>
