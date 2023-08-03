b9147348abb39cd901ba93d4ea689c6b328f2f86
nn-dmt <nn@dmt.pub>
2023-08-03 12:47:36 -0700

    ergopay implementation

--- components/reduceTransaction.ts
+++ components/reduceTransaction.ts
@@ -20,4 +20,42 @@ export class TransactionReducer {
 
   private async getTxReduced(json, inputs, dataInputs): Promise<[string, any]>{
     const unsignedTx =  UnsignedTransaction.from_json(JSONBigInt.stringify(json));
-   
+
+    const inputBoxes = ErgoBoxes.from_boxes_json(inputs);
+    const inputDataBoxes = ErgoBoxes.from_boxes_json(dataInputs);
+
+    let ctx;
+    try {
+        ctx = await this.getErgoStateContext();
+    } catch (e) {
+        console.log("error", e);    
+    }
+
+    const id = unsignedTx.id().to_str();
+    const reducedTx = ReducedTransaction.from_unsigned_tx(unsignedTx, inputBoxes, inputDataBoxes, ctx);
+    return [id, reducedTx];
+  }
+
+  private async getErgoStateContext(): Promise<any> {
+    let explorerHeaders: any = [];
+    try {
+        explorerHeaders = await getExplorerBlockHeaders();
+    } catch (e) {
+        console.log("error", e);
+    }
+
+    const block_headers = BlockHeaders.from_json(explorerHeaders);
+    const pre_header = PreHeader.from_block_header(block_headers.get(0));
+    const ctx = new ErgoStateContext(pre_header, block_headers);
+    return ctx;
+  }
+
+  private byteArrayToBase64(byteArray) {
+    var binary = '';
+    var len = byteArray.byteLength;
+    for (var i = 0; i < len; i++) {
+        binary += String.fromCharCode(byteArray[i]);
+    }
+    return window.btoa(binary);
+  }
+}
