970b2a1fa307a10caa8d317041e8398b2e2ee186
nn-dmt <nn@dmt.pub>
2023-08-05 10:29:49 -0700

    create-release form

--- main.tsx
+++ main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
