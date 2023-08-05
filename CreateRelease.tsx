7c753b2ac7f9dbed8775309167ed500afd134379
nn-dmt <nn@dmt.pub>
2023-08-05 10:45:57 -0700

    create-release form

--- CreateRelease.tsx
+++ CreateRelease.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MyComponent = () => (
   <iframe 
-    src="https://thz.fm/create-release"
+    src="https://thz.fm/me/create-release"
     width="100%"
     height="800px"
   />
