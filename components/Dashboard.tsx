6b10a31d02a9d8a7c65d22a06603ca0937abaf1f
nn-dmt <nn@dmt.pub>
2023-08-06 16:31:03 -0700

    login css

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,13 +1,25 @@
 // Venues.jsx
 import React from 'react';
+import { useFrappeAuth } from 'frappe-react-sdk';
 
 const Dashboard = () => {
+  const {
+    currentUser,
+    isValidating,
+    isLoading,
+    login,
+    logout,
+    error,
+    updateCurrentUser,
+    getUserCookie,
+  } = useFrappeAuth();
+  
   return (
     <div className="content">
       This is the Dashboard page.
 
       <button onClick={logout}>Logout</button>
-      
+
     </div>
   );
 };
