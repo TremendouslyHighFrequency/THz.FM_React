51b7d3fac5542e9884e414eabee3529f9e200495
nn-dmt <nn@dmt.pub>
2023-08-06 16:37:09 -0700

    login css

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -11,8 +11,7 @@ const Dashboard = () => {
     <div className="content">
       {currentUser ? (
         <>
-          <h1>Welcome {currentUser}</h1>
-          <p>This is your Dashboard page.</p>
+          <h1>Welcome to your user dashboard, {currentUser}</h1>
           <button onClick={logout}>Logout</button>
         </>
       ) : (
