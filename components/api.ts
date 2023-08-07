5bf45e380142088ae9e00b5bdc1f009a06bc5382
nn-dmt <nn@dmt.pub>
2023-08-07 12:34:42 -0700

    css

--- components/api.ts
+++ components/api.ts
@@ -20,8 +20,8 @@ export async function getNotifications(loggedUser: string) {
 
 export async function getUserImage(loggedUser: string) {
   try {
-    const response = await axios.get(`https://thz.fm/api/resource/User?fields=["user_image"]&filters=[["User","name","=","${loggedUser}"]]`);
-    console.log("User Image Response:", response.data);  // Add this line for debugging
+    const response = await axios.get(`https://thz.fm/api/resource/User/${loggedUser}?fields=["user_image"]`);
+    console.log("User Image Response:", response.data);  
     return response.data.data.user_image;
   } catch (error) {
     console.error(`Error fetching user data: ${error}`);
