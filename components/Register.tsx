85c84389e82cc73ab6f34bfdd3d8daa2bbc2e3b5
nn-dmt <nn@dmt.pub>
2023-08-10 16:18:01 -0700

    UI/UX

--- components/Register.tsx
+++ components/Register.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useFrappeCreateDoc } from 'frappe-react-sdk'; // assuming this hook exists
 
 export const Register = () => {
   const [username, setUsername] = useState('');
