3c39c91faf914c1e76ddf8956842fd9e3d2da6e6
nn-dmt <nn@dmt.pub>
2023-08-11 10:26:14 -0700

    UI/UX

--- components/SearchResults.jsx
+++ components/SearchResults.jsx
@@ -3,19 +3,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const SearchResults = ({ results }) => {
-  console.log("Results:", results); // Log the entire results array to the console
-
   return (
     <div className="search-results">
       {results.length > 0 && (
         <div className="search-dropdown show">
           {results.map((result, index) => (
             <div key={index} className="navbar-dropdown-item">
-              <Link to={`/releases/${result.title}/by/${result.release_artist}/${result.name}`}>
-                <p>{result.title}</p>
-                <p>{result.release_artist}</p> {/* Display other attributes to see if they are present */}
-                <p>{result.name}</p>
-              </Link>
+              <Link to={`/releases/${result.title}/by/${result.release_artist_primary}/${result.id}`}>
+              <p>{result.title}</p>
+            </Link>
             </div>
           ))}
         </div>
