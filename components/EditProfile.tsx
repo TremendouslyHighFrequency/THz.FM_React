8a8fc6d1574459f1fddcd1bf69e298371c6515ab
nn-dmt <nn@dmt.pub>
2023-08-10 20:10:20 -0700

    UI/UX

---
+++ components/EditProfile.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const EditProfile = () => (
+  <iframe className="form-frame"
+    src="https://thz.fm/me/edit-profile"
+    width="100%"
+  />
+);
+
+export default EditProfile;
