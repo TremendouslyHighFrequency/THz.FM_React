f995bd261d9eafc8ca73ed493d2f48310215f6a1
nn-dmt <nn@dmt.pub>
2023-08-11 08:54:42 -0700

    UI/UX

--- CreateRelease.tsx
+++ CreateRelease.tsx
@@ -64,9 +64,8 @@ const CreateRelease = () => {
                   <Table.Cell>
                     <DropdownMenu.Root>
                       <DropdownMenu.Trigger asChild>
-                        <button className="inline-block">
+                        <button className="inline-block bg-indigo-400 rounded-md px-3 py-2">
                           Manage
-                          <CaretDownIcon />
                         </button>
                       </DropdownMenu.Trigger>
                       <DropdownMenu.Content className="DropdownMenuContent" sideOffset={5}>
