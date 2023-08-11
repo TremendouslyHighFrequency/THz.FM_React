ba3c93c3177d79256889e0d4aecb73a4f08feb1d
nn-dmt <nn@dmt.pub>
2023-08-10 20:48:38 -0700

    UI/UX

--- components/NotificationDropdown.tsx
+++ components/NotificationDropdown.tsx
@@ -31,33 +31,37 @@ const NotificationDropdown = ({ notifications, buttonRef, dropdownVisible, setDr
         right: `${window.innerWidth - buttonRef.current.getBoundingClientRect().right}px`,
         top: `${buttonRef.current.getBoundingClientRect().top + buttonRef.current.getBoundingClientRect().height}px`,
       } : {}}>
-        {notifications && notifications.map((notification, index) => {
-          let actionText;
-          switch (notification.type) {
-            case 'Share':
-              actionText = `has shared ${notification.document_name}`;
-              break;
-            case 'Like':
-              actionText = `has liked ${notification.document_name}`;
-              break;
-            case 'Comment':
-              actionText = `has commented on ${notification.document_name}`;
-              break;
-            default:
-              actionText = notification.type;
-          }
-          return (
-            <div key={index} className="notification-item">
-              <div className="notification">
-                <img className="notification-image" src={notification.user_image || THZIcon} alt="user_image" />
-                <span className="notification-text">{notification.from_user}</span>
-                <span>{actionText}</span>
-                <span>{notification.email_content}</span>
-                {/* Add more fields as needed */}
+        {notifications.length === 0 ? (
+          <div className="no-notifications">There are no new notifications.</div>
+        ) : (
+          notifications.map((notification, index) => {
+            let actionText;
+            switch (notification.type) {
+              case 'Share':
+                actionText = `has shared ${notification.document_name}`;
+                break;
+              case 'Like':
+                actionText = `has liked ${notification.document_name}`;
+                break;
+              case 'Comment':
+                actionText = `has commented on ${notification.document_name}`;
+                break;
+              default:
+                actionText = notification.type;
+            }
+            return (
+              <div key={index} className="notification-item">
+                <div className="notification">
+                  <img className="notification-image" src={notification.user_image || THZIcon} alt="user_image" />
+                  <span className="notification-text">{notification.from_user}</span>
+                  <span>{actionText}</span>
+                  <span>{notification.email_content}</span>
+                  {/* Add more fields as needed */}
+                </div>
               </div>
-            </div>
-          )
-        })}
+            )
+          })
+        )}
         <div className="dropdown-footer">
           <button className="view-notifications" onClick={() => setDropdownVisible(false)}>View all</button>
           <button className="close-notifications-item" onClick={() => setDropdownVisible(false)}><XIcon /> Close</button>
