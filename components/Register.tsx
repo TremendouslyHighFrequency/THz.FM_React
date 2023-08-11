a13135d8455b0e10a48f1b33ececafeeaab1290a
nn-dmt <nn@dmt.pub>
2023-08-10 18:47:48 -0700

    UI/UX

--- components/Register.tsx
+++ components/Register.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 
 export const Register = () => {
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [new_password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
