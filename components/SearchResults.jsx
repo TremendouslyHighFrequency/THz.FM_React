4b0d3d17f95adc918d8fe2ea20e8a2c923967ea1
nn-dmt <nn@dmt.pub>
2023-08-11 10:13:11 -0700

    UI/UX

--- components/SearchResults.jsx
+++ components/SearchResults.jsx
@@ -1,5 +1,6 @@
 // SearchResults.jsx
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SearchResults = ({ results }) => {
   return (
@@ -8,7 +9,9 @@ const SearchResults = ({ results }) => {
         <div className="search-dropdown show">
           {results.map((result, index) => (
             <div key={index} className="navbar-dropdown-item">
+              <Link to={`/releases/${title}/by/${release_artist}/${name}`}>
               <p>{result.title}</p>
+            </Link>
             </div>
           ))}
         </div>
