c5e0ad8e017bf52080e24b3625315c431a7e6e7d
nn-dmt <nn@dmt.pub>
2023-08-10 19:57:58 -0700

    UI/UX

--- CreateRelease.tsx
+++ CreateRelease.tsx
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useFrappeGetDocList } from 'frappe-react-sdk';
+import { ReleaseItem } from '../types'; // Define the ReleaseItem type according to the Release document structure
+import { Link } from "react-router-dom";
+import { getLoggedUser } from './components/api'; // Update with the correct path
 
-const CreateRelease = () => (
-  <iframe className="form-frame"
-    src="https://thz.fm/me/create-release"
-    width="100%"
-  />
-);
+const ManageReleases = () => {
+  const [pageIndex, setPageIndex] = useState<number>(0);
+  const [loggedUser, setLoggedUser] = useState<string | null>(null);
 
-export default CreateRelease;
+  useEffect(() => {
+    getLoggedUser()
+      .then((user) => setLoggedUser(user))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const { data, error, isValidating } = useFrappeGetDocList<ReleaseItem>('Release', {
+    fields: ["title", "release_photo"], // Update the fields as per the 'Release' document structure
+    filters: loggedUser ? { "owner": loggedUser } : {},
+    limit_start: pageIndex,
+    limit: 50,
+    orderBy: {
+      field: "creation",
+      order: 'desc'
+    }
+  });
+
+  if (isValidating) {
+    return <>Loading</>;
+  }
+  if (error) {
+    return <>{JSON.stringify(error)}</>;
+  }
+  if (data && Array.isArray(data)) {
+    return (
+      <div className="releases-index">
+        {
+          data.map(({ title, release_photo }, i) => (
+            <div key={i} className="release-card" style={{ backgroundImage: `url(${encodeURI(release_photo)})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+              <div className="release-text">
+                <h4>{title}</h4>
+                <Link to={`/release/${title}`}>View Release</Link>
+              </div>
+            </div>
+          ))
+        }
+        <button onClick={() => setPageIndex(pageIndex + 50)}>Next page</button>
+      </div>
+    );
+  }
+  return null;
+};
+
+export default ManageReleases;
