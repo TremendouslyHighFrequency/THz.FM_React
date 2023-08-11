88170f0917820fd2cf8c9f98827c662eda69d151
nn-dmt <nn@dmt.pub>
2023-08-11 16:11:34 -0700

    UI/UX

--- components/FrontSideNav.tsx
+++ components/FrontSideNav.tsx
@@ -2,40 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-// Import your components
-import Artists from './Artists';
-import Publishers from './Publishers';
-import Labels from './Labels';
-import Releases from './Releases';
-import Tracks from './Tracks';
-import Events from './Events';
-import Venues from './Venues';
-import Marketplace from './Marketplace';
-import Dashboard from './Dashboard';
+// Import all components
+import * as Components from './Components';
 
 type SidebarItem = {
   title: string;
   route: string;
   component?: React.ComponentType<any>;
 };
-
-type SidebarData = {
-  sidebar_items: SidebarItem[];
-};
-
-// // Create a mapping of routes to components
-// const routeComponents: Record<string, React.ComponentType<any>> = {
-//   '/artists': Artists,
-//   '/publishers': Publishers,
-//   '/labels': Labels,
-//   '/releases': Releases,
-//   '/tracks': Tracks,
-//   '/events': Events,
-//   '/venues': Venues,
-//   '/z': Marketplace,
-//   '/dashboard': Dashboard,
-//   // add the route for labels and the other routes here...
-// };
 
 const SideNav = () => {
   const [navItems, setNavItems] = useState<SidebarItem[]>([]);
@@ -44,11 +18,11 @@ const SideNav = () => {
     axios.get('https://thz.fm/api/resource/Website%20Sidebar/Main%20Website%20Menu')
       .then(response => {
         // Extract the sidebar_items array from the data
-        const sidebarData: SidebarData = response.data.data;
-        // Transform the sidebar items to include the corresponding component
-        const newNavItems = sidebarData.sidebar_items.map(item => ({
+        const sidebarData = response.data.data.sidebar_items;
+        // Map the sidebar items to the corresponding components
+        const newNavItems = sidebarData.map(item => ({
           ...item,
-          component: routeComponents[item.route],
+          component: Components[item.title.replace(/\s+/g, '')],
         }));
         setNavItems(newNavItems);
       })
@@ -57,17 +31,15 @@ const SideNav = () => {
       });
   }, []);
 
-  return {navItems, links: (
+  return (
     <div className="side-navContainer">
-     <div className="side-nav">
-      {navItems.map(item => (
-        <li key={item.route}><Link to={item.route}>{item.title}</Link></li>
-      ))}
-      </div>
+      <ul className="side-nav">
+        {navItems.map(item => (
+          <li key={item.route}><Link to={item.route}>{item.title}</Link></li>
+        ))}
+      </ul>
     </div>
-  )};
+  );
 };
 
 export default SideNav;
-export { SidebarItem, routeComponents };
-
