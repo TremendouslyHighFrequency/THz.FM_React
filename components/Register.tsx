16fdea9a2b0956725823547f1541a2bc2cb36e15
nn-dmt <nn@dmt.pub>
2023-08-10 18:39:01 -0700

    UI/UX

--- components/Register.tsx
+++ components/Register.tsx
@@ -10,7 +10,8 @@ export const Register = () => {
   const handleRegister = () => {
     setLoading(true);
     const userData = {
-      email: email
+      email: email,
+      password: password
     };
 
     fetch(`https://thz.fm/api/resource/User`, {
@@ -50,6 +51,15 @@ return (
                 </div>
               </div>
    
+              <div className="border-2 border-solid rounded flex items-center mb-4">
+                <div className="w-10 h-10 flex justify-center items-center flex-shrink-0">
+                  <span className="fas fa-lock text-gray-500"></span>
+                </div>
+                <div className="flex-1">
+                  <input type="password" placeholder="Password" className="h-10 py-1 pr-3 w-full" value={password} onChange={e => setPassword(e.target.value)}></input>
+                </div>
+              </div>
+
               <p className="text-sm text-center mt-6">By signing up, you agree to our <a href="#" className="text-indigo-600 hover:underline">Terms</a> and <a href="#" className="text-indigo-600 hover:underline">Privacy Policy</a></p>
               <div className="text-center mt-6 md:mt-12">
               <button className="bg-indigo-600 hover:bg-indigo-700 text-white text-xl py-2 px-4 md:px-6 rounded transition-colors duration-300" onClick={handleRegister} disabled={loading}>Sign Up <span className="far fa-paper-plane ml-2"></span></button>
