ad30944cb45589a95331fe9c8995d79d13565c0e
nn-dmt <nn@dmt.pub>
2023-08-12 10:04:02 -0700

    css

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -78,7 +78,7 @@ const LoginModal = ({ onSuccessfulLogin }) => {
               />
             </div>
             <div className="flex items-center justify-between mt-4">
-              <button onClick={handleLogin} className="w-full px-6 py-2.5 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
+              <button onClick={handleLogin} className="w-full bg-indigo-600 px-6 py-2.5 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
                 Sign In
               </button>
             </div>
