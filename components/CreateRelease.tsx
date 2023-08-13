aadeb96e45628934b14a6083351c44829cb2cd6f
nn-dmt <nn@dmt.pub>
2023-08-12 20:16:42 -0700

    css

--- components/CreateRelease.tsx
+++ components/CreateRelease.tsx
@@ -286,7 +286,7 @@ const deleteSelectedTracks = () => {
       </div>
         {/* Submit Button */}
         <div className="flex justify-end mt-6">
-          <button className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Save</button>
+          <button onClick={handleSubmit} className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Save</button>
         </div>
       </form>
     </div>
