33f7233a41a498d76f4f0868777cea18cdb33b93
nn-dmt <nn@dmt.pub>
2023-08-12 18:43:50 -0700

    css

--- components/CreateRelease.tsx
+++ components/CreateRelease.tsx
@@ -1,53 +1,82 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 
 const CreateRelease = () => {
-  const [userArtists, setUserArtists] = useState([]);
-  const [loggedUser, setLoggedUser] = useState('');
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const user = await getLoggedUser();
-      setLoggedUser(user);
-      // Fetch user artists here and set to userArtists
-      // Example: setUserArtists(await fetchUserArtists(user));
-    };
-    fetchData();
-  }, []);
-
   return (
     <div className="content">
       <form>
-        {/* Release fields */}
         <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
-          {/* ... other fields ... */}
+          {/* Release Main Details */}
           <div>
             <label className="text-gray-700 dark:text-gray-200" htmlFor="title">Title</label>
             <input id="title" type="text" className="block w-full px-4 py-2 mt-2" />
           </div>
-          {/* ... */}
+          <div>
+            <label className="text-gray-700 dark:text-gray-200" htmlFor="release_id">Release ID</label>
+            <input id="release_id" type="text" className="block w-full px-4 py-2 mt-2" />
+          </div>
           <div>
             <label className="text-gray-700 dark:text-gray-200" htmlFor="release_artist">Release Artist</label>
-            <select id="release_artist" className="block w-full px-4 py-2 mt-2">
-              {userArtists.map(artist => (
-                <option key={artist.id} value={artist.name}>{artist.name}</option>
-              ))}
-            </select>
+            <input id="release_artist" type="text" className="block w-full px-4 py-2 mt-2" />
           </div>
-          {/* ... */}
-        </div>
-
-        {/* Release Tracks fields */}
-        <h2>Tracks</h2>
-        <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
-          {/* Fields for track 1 */}
           <div>
-            <label className="text-gray-700 dark:text-gray-200" htmlFor="track_title">Track Title</label>
-            <input id="track_title" type="text" className="block w-full px-4 py-2 mt-2" />
+            <label className="text-gray-700 dark:text-gray-200" htmlFor="release_label">Release Label</label>
+            <input id="release_label" type="text" className="block w-full px-4 py-2 mt-2" />
           </div>
-          {/* ... other track fields ... */}
+          <div>
+            <label className="text-gray-700 dark:text-gray-200" htmlFor="release_type">Release Type</label>
+            <input id="release_type" type="text" className="block w-full px-4 py-2 mt-2" />
+          </div>
+          <div>
+            <label className="text-gray-700 dark:text-gray-200" htmlFor="release_artwork">Release Artwork URL</label>
+            <input id="release_artwork" type="url" className="block w-full px-4 py-2 mt-2" />
+          </div>
+          <div>
+            <label className="text-gray-700 dark:text-gray-200" htmlFor="release_description">Release Description</label>
+            <textarea id="release_description" className="block w-full px-4 py-2 mt-2"></textarea>
+          </div>
+          <div>
+            <label className="text-gray-700 dark:text-gray-200" htmlFor="release_date">Release Date</label>
+            <input id="release_date" type="date" className="block w-full px-4 py-2 mt-2" />
+          </div>
+          <div>
+            <label className="text-gray-700 dark:text-gray-200" htmlFor="price_usd">Price (USD)</label>
+            <input id="price_usd" type="number" step="0.01" className="block w-full px-4 py-2 mt-2" />
+          </div>
+          <div>
+            <label className="text-gray-700 dark:text-gray-200" htmlFor="price_erg">Price (ERG)</label>
+            <input id="price_erg" type="number" step="0.01" className="block w-full px-4 py-2 mt-2" />
+          </div>
+          <div>
+            <label className="text-gray-700 dark:text-gray-200" htmlFor="zip_file_path">ZIP File Path</label>
+            <input id="zip_file_path" type="text" className="block w-full px-4 py-2 mt-2" />
+          </div>
+          {/* ... Additional fields ... */}
         </div>
 
+        {/* Track Details */}
+        <h2 className="mt-4">Tracks</h2>
+        {/* For simplicity, I'm including fields for only one track. */}
+        <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+          <div>
+            <label className="text-gray-700 dark:text-gray-200" htmlFor="track_title_1">Track Title</label>
+            <input id="track_title_1" type="text" className="block w-full px-4 py-2 mt-2" />
+          </div>
+          <div>
+            <label className="text-gray-700 dark:text-gray-200" htmlFor="track_artist_1">Track Artist</label>
+            <input id="track_artist_1" type="text" className="block w-full px-4 py-2 mt-2" />
+          </div>
+          <div>
+            <label className="text-gray-700 dark:text-gray-200" htmlFor="track_type_1">Track Type</label>
+            <input id="track_type_1" type="text" className="block w-full px-4 py-2 mt-2" />
+          </div>
+          <div>
+            <label className="text-gray-700 dark:text-gray-200" htmlFor="attach_mp3_1">Attach MP3</label>
+            <input id="attach_mp3_1" type="file" className="block w-full px-4 py-2 mt-2" />
+          </div>
+          {/* ... Additional fields for track ... */}
+        </div>
+
+        {/* Submit Button */}
         <div className="flex justify-end mt-6">
           <button className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Save</button>
         </div>
