681b3908a98bb63cd176a92e906dfda6dc9eab8d
nn-dmt <nn@dmt.pub>
2023-08-12 17:09:52 -0700

    css

--- ManageReleases.tsx
+++ ManageReleases.tsx
@@ -91,9 +91,11 @@ const ManageReleases = () => {
                       <Table.Cell>
                         <DropdownMenu.Root>
                           <DropdownMenu.Trigger asChild>
-                            <button className="inline-block bg-indigo-400 rounded-md px-2 py-1 text-sm my-2 shadow-sm">
-                              Manage
-                            </button>
+                          <button class="px-1 py-1 text-gray-500 transition-colors duration-200 rounded-lg dark:text-gray-300 hover:bg-gray-100">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
+                                        </svg>
+                                    </button>
                           </DropdownMenu.Trigger>
                           <DropdownMenu.Content className="DropdownMenuContent" sideOffset={5}>
                             <DropdownMenu.Item className="DropdownMenuItem">Edit</DropdownMenu.Item>
