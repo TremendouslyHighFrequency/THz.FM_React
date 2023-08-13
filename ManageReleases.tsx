662a7887044ce64242d66f5521b213bbed4dc7bb
nn-dmt <nn@dmt.pub>
2023-08-12 21:05:28 -0700

    css

--- ManageReleases.tsx
+++ ManageReleases.tsx
@@ -46,9 +46,9 @@ const ManageReleases = () => {
         <div className="publishedReleases">
           <section className="container px-4 mx-auto">
             <div className="sm:flex sm:items-center sm:justify-between">
-              <h2 className="text-lg font-medium text-gray-800 dark:text-white">Releases:</h2>
+              <h2 className="text-lg font-medium text-gray-800">Releases:</h2>
               <div className="flex items-center mt-4 gap-x-3">
-              <button className="flex items-center justify-center w-1/2 m-6 px-5 py-2 text-sm tracking-wide shadow-sm text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto gap-x-2 hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
+              <button className="flex items-center justify-center w-1/2 m-6 px-5 py-2 text-sm tracking-wide shadow-sm text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto gap-x-2 hover:bg-blue-600 ">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
@@ -58,7 +58,7 @@ const ManageReleases = () => {
             </div>
           </section>
           <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-            <div className="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
+            <div className="overflow-hidden border border-gray-200 md:rounded-lg">
               <Table.Root className="min-w-full divide-y divide-gray-200">
                 <Table.Header className="bg-gray-50">
                   <Table.Row className="px-12 bg-gray-50">
@@ -91,7 +91,7 @@ const ManageReleases = () => {
                       <Table.Cell className="align-middle text-center">
                         <DropdownMenu.Root>
                           <DropdownMenu.Trigger asChild>
-                          <button class="px-1 py-1 text-gray-500 transition-colors duration-200 rounded-lg dark:text-gray-300 hover:bg-gray-100">
+                          <button class="px-1 py-1 text-gray-500 transition-colors duration-200 rounded-lg hover:bg-gray-100">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
                                         </svg>
@@ -118,18 +118,18 @@ const ManageReleases = () => {
     );
   } if (data && Array.isArray(data) && data.length === 0) {
     return (
-      <div className="flex items-center mt-6 text-center border rounded-lg h-96 dark:border-gray-700">
+      <div className="flex items-center mt-6 text-center border rounded-lg h-96">
         <div className="flex flex-col w-full max-w-sm px-4 mx-auto">
-          <div className="p-3 mx-auto text-blue-500 bg-blue-100 rounded-full dark:bg-gray-800">
+          <div className="p-3 mx-auto text-blue-500 bg-blue-100 rounded-full ">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
             </svg>
           </div>
-          <h1 className="mt-3 text-lg text-gray-800 dark:text-white">No releases found</h1>
-          <p className="mt-2 text-gray-500 dark:text-gray-400">You don't have any releases. Click the button to add one.</p>
+          <h1 className="mt-3 text-lg text-gray-800 ">No releases found</h1>
+          <p className="mt-2 text-gray-500 ">You don't have any releases. Click the button to add one.</p>
           <div className="flex items-center mt-4 sm:mx-auto gap-x-3">
            
-            <button className="flex items-center justify-center w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto gap-x-2 hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
+            <button className="flex items-center justify-center w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto gap-x-2 hover:bg-blue-600 ">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
