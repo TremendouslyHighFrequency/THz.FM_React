d9954881f5b1ec63c9faa0c0bbcebad009f19848
nn-dmt <nn@dmt.pub>
2023-08-12 18:51:42 -0700

    css

--- components/CreateRelease.tsx
+++ components/CreateRelease.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const CreateRelease = () => {
+  const [tracks, setTracks] = useState([]);
+
+  const addTrack = () => {
+    // Adding an empty track object to the tracks array
+    setTracks([...tracks, {}]);
+  };
+
   return (
     <div className="content">
       <form>
@@ -53,28 +60,40 @@ const CreateRelease = () => {
           {/* ... Additional fields ... */}
         </div>
 
-        {/* Track Details */}
-        <h2 className="mt-4">Tracks</h2>
-        {/* For simplicity, I'm including fields for only one track. */}
-        <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
-          <div>
-            <label className="text-gray-700 dark:text-gray-200" htmlFor="track_title_1">Track Title</label>
-            <input id="track_title_1" type="text" className="block w-full px-4 py-2 mt-2" />
-          </div>
-          <div>
-            <label className="text-gray-700 dark:text-gray-200" htmlFor="track_artist_1">Track Artist</label>
-            <input id="track_artist_1" type="text" className="block w-full px-4 py-2 mt-2" />
-          </div>
-          <div>
-            <label className="text-gray-700 dark:text-gray-200" htmlFor="track_type_1">Track Type</label>
-            <input id="track_type_1" type="text" className="block w-full px-4 py-2 mt-2" />
-          </div>
-          <div>
-            <label className="text-gray-700 dark:text-gray-200" htmlFor="attach_mp3_1">Attach MP3</label>
-            <input id="attach_mp3_1" type="file" className="block w-full px-4 py-2 mt-2" />
-          </div>
-          {/* ... Additional fields for track ... */}
-        </div>
+         {/* Tracks Table */}
+         <h2 className="mt-4">Tracks</h2>
+        <table className="min-w-full border">
+          <thead>
+            <tr>
+              {/* Table headers based on track fields */}
+              <th className="border px-4 py-2">Title</th>
+              <th className="border px-4 py-2">Track Number</th>
+              <th className="border px-4 py-2">Artist</th>
+              <th className="border px-4 py-2">Type</th>
+              <th className="border px-4 py-2">MP3</th>
+              <th className="border px-4 py-2">Price (USD)</th>
+              <th className="border px-4 py-2">Price (ERG)</th>
+              <th className="border px-4 py-2">Route</th>
+              <th className="border px-4 py-2">Published</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tracks.map((_, idx) => (
+              <tr key={idx}>
+                <td className="border px-4 py-2"><input type="text" /></td>
+                <td className="border px-4 py-2"><input type="number" /></td>
+                <td className="border px-4 py-2"><input type="text" /></td>
+                <td className="border px-4 py-2"><input type="text" /></td>
+                <td className="border px-4 py-2"><input type="file" /></td>
+                <td className="border px-4 py-2"><input type="number" step="0.01" /></td>
+                <td className="border px-4 py-2"><input type="number" step="0.01" /></td>
+                <td className="border px-4 py-2"><input type="text" /></td>
+                <td className="border px-4 py-2"><input type="checkbox" /></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <button type="button" onClick={addTrack} className="mt-4 px-4 py-2 bg-blue-500 text-white rounded">Add Track</button>
 
         {/* Submit Button */}
         <div className="flex justify-end mt-6">
