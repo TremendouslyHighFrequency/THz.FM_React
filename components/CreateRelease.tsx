797abf022b7299e9ad70a9bf4e64ccec686fd334
nn-dmt <nn@dmt.pub>
2023-08-12 19:45:55 -0700

    css

--- components/CreateRelease.tsx
+++ components/CreateRelease.tsx
@@ -1,8 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useFrappeGetDocList } from 'frappe-react-sdk';
+import { getLoggedUser } from './api';
 
 const CreateRelease = () => {
   const [tracks, setTracks] = useState([]);
-  const [selectedTracks, setSelectedTracks] = useState({});  
+  const [selectedTracks, setSelectedTracks] = useState({});
+  const [labels, setLabels] = useState([]);
+  const [artists, setArtists] = useState([]);
+  const [releaseTypes, setReleaseTypes] = useState([]);
+  const [loggedUser, setLoggedUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [fetchError, setFetchError] = useState(null);
+
+  // Fetch the logged-in user
+  useEffect(() => {
+    getLoggedUser().then(user => setLoggedUser(user));
+  }, []);
+
+  // Fetch labels owned by the logged-in user
+  const { data: userLabels } = useFrappeGetDocList('Label', {
+    fields: ["title"],
+    filters: loggedUser ? { "owner": loggedUser } : null, // Filter by the owner
+    limit: 50,
+    orderBy: {
+      field: "creation",
+      order: 'desc'
+    }
+  });
+  
+  // Then, within your useEffect for fetching labels:
+  useEffect(() => {
+      if (userLabels) {
+        setLabels(userLabels.map(label => label.title));
+        setIsLoading(false);
+      } else if (error) {
+        setFetchError(error);
+        setIsLoading(false);
+      }
+  }, [userLabels, error]);
 
   const addTrack = () => {
     // Adding a track object with auto-incremented track_number to the tracks array
@@ -33,18 +68,29 @@ const deleteSelectedTracks = () => {
             <input id="release_id" type="text" className="block w-full px-4 py-2 mt-2" />
           </div>
           <div>
-            <label className="text-gray-700 dark:text-gray-200" htmlFor="release_artist">Release Artist</label>
-            <input id="release_artist" type="text" className="block w-full px-4 py-2 mt-2" />
-          </div>
-          <div>
-            <label className="text-gray-700 dark:text-gray-200" htmlFor="release_label">Release Label</label>
-            <input id="release_label" type="text" className="block w-full px-4 py-2 mt-2" />
-          </div>
-          <div>
-            <label className="text-gray-700 dark:text-gray-200" htmlFor="release_type">Release Type</label>
-            <input id="release_type" type="text" className="block w-full px-4 py-2 mt-2" />
-          </div>
-          <div>
+          <label className="text-gray-700 dark:text-gray-200" htmlFor="release_artist">Release Artist</label>
+          <select id="release_artist" className="block w-full px-4 py-2 mt-2">
+            {artists.map(artist => (
+              <option key={artist} value={artist}>{artist}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label className="text-gray-700 dark:text-gray-200" htmlFor="release_label">Release Label</label>
+          <select id="release_label" className="block w-full px-4 py-2 mt-2">
+            {labels.map(label => (
+              <option key={label} value={label}>{label}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label className="text-gray-700 dark:text-gray-200" htmlFor="release_type">Release Type</label>
+          <select id="release_type" className="block w-full px-4 py-2 mt-2">
+            {releaseTypes.map(type => (
+              <option key={type} value={type}>{type}</option>
+            ))}
+          </select>
+        </div>
             <label className="text-gray-700 dark:text-gray-200" htmlFor="release_artwork">Release Artwork URL</label>
             <input id="release_artwork" type="url" className="block w-full px-4 py-2 mt-2" />
           </div>
@@ -65,12 +111,12 @@ const deleteSelectedTracks = () => {
             <input id="price_erg" type="number" step="0.01" className="block w-full px-4 py-2 mt-2" />
           </div>
           <div>
-            <label className="text-gray-700 dark:text-gray-200" htmlFor="price_erg">Ergo Address</label>
-            <input id="release_ergo_address" type="number" step="0.01" className="block w-full px-4 py-2 mt-2" />
+            <label className="text-gray-700 dark:text-gray-200" htmlFor="release_ergo_address">Ergo Address</label>
+            <input id="release_ergo_address" type="text" step="0.01" className="block w-full px-4 py-2 mt-2" />
           </div>
           {/* ... Additional fields ... */}
         </div>
-
+</div>
          {/* Tracks Table */}
          <h2 className="mt-4">Tracks</h2>
         <table className="min-w-full border">
@@ -123,12 +169,12 @@ const deleteSelectedTracks = () => {
         }} />
       </td>
       <td className="border">
-        <input type="file" name="attach_wav" value={track.attach_wav || ''} onChange={(e) => {
-          let newTracks = [...tracks];
-          newTracks[idx].track_type = e.target.value;
-          setTracks(newTracks);
-        }} />
-      </td>
+  <input type="file" name="attach_wav" value={track.attach_wav || ''} onChange={(e) => {
+    let newTracks = [...tracks];
+    newTracks[idx].attach_wav = e.target.value; // Corrected here
+    setTracks(newTracks);
+  }} />
+</td>
       <td className="border">
         <input type="number" step="0.01" name="price_usd" value={track.price_usd || 0} onChange={(e) => {
           let newTracks = [...tracks];
