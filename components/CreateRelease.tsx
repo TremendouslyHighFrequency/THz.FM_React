e684d0fda9e3c76ca7ca05fad31eb3237f029d02
nn-dmt <nn@dmt.pub>
2023-08-12 18:32:31 -0700

    css

--- components/CreateRelease.tsx
+++ components/CreateRelease.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React from 'react'; // This line can be omitted if you're using React version 17 or above.
 
-export const CreateRelease = () => {
+const CreateRelease = () => {
   return (
     <div className="content">
-      This is the create release page.
+       <form>
+        <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+            <div>
+                <label className="text-gray-700 dark:text-gray-200" htmlFor="username">Username</label>
+                <input id="username" type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+            </div>
+
+            <div>
+                <label className="text-gray-700 dark:text-gray-200" htmlFor="emailAddress">Email Address</label>
+                <input id="emailAddress" type="email" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+            </div>
+
+            <div>
+                <label className="text-gray-700 dark:text-gray-200" htmlFor="password">Password</label>
+                <input id="password" type="password" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+            </div>
+
+            <div>
+                <label className="text-gray-700 dark:text-gray-200" htmlFor="passwordConfirmation">Password Confirmation</label>
+                <input id="passwordConfirmation" type="password" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+            </div>
+        </div>
+
+        <div className="flex justify-end mt-6">
+            <button className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Save</button>
+        </div>
+    </form>
     </div>
   );
 };
-
 
 export default CreateRelease;
