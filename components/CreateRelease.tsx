404ab7f895a8502b70ef27548424b1f82a3485a5
nn-dmt <nn@dmt.pub>
2023-08-12 20:21:27 -0700

    css

--- components/CreateRelease.tsx
+++ components/CreateRelease.tsx
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { useFrappeGetDocList, useFrappeCreateDoc } from 'frappe-react-sdk';
 import { getLoggedUser } from './api';
 
-const createDoc = useFrappeCreateDoc();
-
 const CreateRelease = () => {
   const [tracks, setTracks] = useState([]);
   const [selectedTracks, setSelectedTracks] = useState({});
@@ -81,7 +79,7 @@ const handleSubmit = (e) => {
     release_tracks: formattedTracks
   };
 
-  createDoc('Release', releaseData)
+  useFrappeCreateDoc('Release', releaseData)
     .then(doc => {
       console.log("Successfully created release:", doc);
       // Consider resetting your component state here or redirecting the user
