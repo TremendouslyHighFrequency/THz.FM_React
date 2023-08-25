6b468009ed75be844de2a9f39af1d1d02027dc9e
nn-dmt <nn@dmt.pub>
2023-08-24 18:34:28 -0700

    listening party

--- components/ChatBox.tsx
+++ components/ChatBox.tsx
@@ -1,30 +1,52 @@
 import React, { useState, useEffect, useRef } from 'react';
-import io from 'socket.io-client';
 
 const ChatBox = () => {
-  const [message, setMessage] = useState('');
-  const [chat, setChat] = useState([]);
-  const socket = io('https://thz.fm:9000', { transports: ['websocket'] });
-  const messagesEndRef = useRef(null);
-  const chatBoxRef = useRef(null);
+    const [message, setMessage] = useState('');
+    const [chat, setChat] = useState([]);
+    const chatBoxRef = useRef(null);
+    const messagesEndRef = useRef(null);
 
-  useEffect(() => {
-    socket.on('receive-message', (msg) => {
-        console.log("Received message:", msg);
-      const isAtBottom = chatBoxRef.current.scrollHeight - chatBoxRef.current.scrollTop === chatBoxRef.current.clientHeight;
-      setChat(prevChat => [...prevChat, msg]);
-      if (isAtBottom) {
-        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+useEffect(() => {
+    // Establish a socket connection using the native WebSocket API
+    const socket = new WebSocket('wss://thz.fm/socket.io/'); // Adjust the URL if needed
+
+    socket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      if (data.event === 'receive-message') {
+        const msg = data.message;
+        const isAtBottom = chatBoxRef.current.scrollHeight - chatBoxRef.current.scrollTop === chatBoxRef.current.clientHeight;
+        setChat(prevChat => [...prevChat, msg]);
+        if (isAtBottom) {
+          messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+        }
       }
-    });
-    return () => socket.disconnect();
-  }, []);
+    };
 
-  const sendMessage = () => {
-    console.log("Sending message:", message);
-    socket.emit('send-message', message);
-    setMessage('');
-  }
+    return () => socket.close(); // Close the socket connection on component unmount
+}, []);
+
+const sendMessage = () => {
+    // Use standard fetch to make an API call to Frappe to broadcast the message
+    fetch('https://thz.fm/api/method/frappe.socketio.send_message?message=' + encodeURIComponent(message), {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.status === "Message broadcasted successfully") {
+        setMessage(''); // Clear input only if message was successfully sent
+      } else {
+        console.error("Error sending message:", data.error);
+      }
+    })
+    .catch(error => {
+        console.error("Error calling the API:", error);
+    });
+}
+
 
   return (
     <div>
