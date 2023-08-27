1e28f212648507d6b38dd2a119574b3373e791d7
nn-dmt <nn@dmt.pub>
2023-08-26 17:44:43 -0700

    ui stuff

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-
+import HomeFeature from './FeaturedHomeBlock';
 
 const Home = () => {
 
   return (
     <div className="m-18">
-  
+  <HomeFeature />
     </div>
   );
 };
