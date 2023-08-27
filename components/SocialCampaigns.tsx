3b95f8dd045be6af7235ec5e532d22e073071df3
nn-dmt <nn@dmt.pub>
2023-08-26 17:12:05 -0700

    ui stuff

--- components/SocialCampaigns.tsx
+++ components/SocialCampaigns.tsx
@@ -13,115 +13,88 @@ import {
 } from "@tremor/react";
 import { useState } from "react";
 
-type Publisher = {
+type MusicLabel = {
     name: string;
-    booksPublished: number;
-    bestSeller: string;
-    averageRating: string;
+    albumsReleased: number;
+    topTrack: string;
+    topTrackRating: string;
     country: string;
     status: string;
     deltaType: DeltaType;
   };
   
 
-  const publishers: Publisher[] = [
+  const musicLabels: MusicLabel[] = [
     {
-      name: "Alpha Publishers",
-      booksPublished: 150,
-      bestSeller: "Mystery of the Mountain",
-      averageRating: "4.5",
+      name: "Alpha Records",
+      albumsReleased: 20,
+      topTrack: "Echoes of Silence",
+      topTrackRating: "4.8",
       country: "USA",
-      status: "leading",
+      status: "renowned",
       deltaType: "increase",
     },
     {
-        name: "Beta Publishers",
-        booksPublished: 150,
-        bestSeller: "Mystery of the Mountain",
-        averageRating: "4.5",
-        country: "USA",
-        status: "leading",
-        deltaType: "increase",
-      },
-      {
-        name: "Gamma Publishers",
-        booksPublished: 150,
-        bestSeller: "Mystery of the Mountain",
-        averageRating: "4.5",
-        country: "USA",
-        status: "leading",
-        deltaType: "increase",
-      },
-      {
-        name: "Sigma Publishers",
-        booksPublished: 150,
-        bestSeller: "Mystery of the Mountain",
-        averageRating: "4.5",
-        country: "USA",
-        status: "leading",
-        deltaType: "increase",
-      },
-      {
-        name: "Theta Publishers",
-        booksPublished: 150,
-        bestSeller: "Mystery of the Mountain",
-        averageRating: "4.5",
-        country: "USA",
-        status: "leading",
-        deltaType: "increase",
-      },
+      name: "Beta Sounds",
+      albumsReleased: 15,
+      topTrack: "Moonlight Serenade",
+      topTrackRating: "4.5",
+      country: "UK",
+      status: "emerging",
+      deltaType: "unchanged",
+    },
 ];
 
-export default function SocialCampaigns() {
-    const [selectedPublishers, setSelectedPublishers] = useState<string[]>([]);
+export default function MusicCampaigns() {
+    const [selectedLabels, setSelectedLabels] = useState<string[]>([]);
   
-    const isPublisherSelected = (publisher: Publisher) =>
-      selectedPublishers.includes(publisher.name) || selectedPublishers.length === 0;
+    const isLabelSelected = (label: MusicLabel) =>
+      selectedLabels.includes(label.name) || selectedLabels.length === 0;
   
-      return (
-        <Card>
-          <MultiSelect
-            onValueChange={setSelectedPublishers}
-            placeholder="Select Publisher..."
-            className="max-w-xs"
-          >
-            {publishers.map((item) => (
-              <MultiSelectItem key={item.name} value={item.name}>
-                {item.name}
-              </MultiSelectItem>
-            ))}
-          </MultiSelect>
-          <Table className="mt-6">
-            <TableHead>
-              <TableRow>
-                <TableHeaderCell>Name</TableHeaderCell>
-                <TableHeaderCell className="text-right">Books Published</TableHeaderCell>
-                <TableHeaderCell className="text-right">Best Seller</TableHeaderCell>
-                <TableHeaderCell className="text-right">Average Rating</TableHeaderCell>
-                <TableHeaderCell className="text-right">Country</TableHeaderCell>
-                <TableHeaderCell className="text-right">Status</TableHeaderCell>
-              </TableRow>
-            </TableHead>
-    
-            <TableBody>
-              {publishers
-                .filter((item) => isPublisherSelected(item))
-                .map((item) => (
-                  <TableRow key={item.name}>
-                    <TableCell>{item.name}</TableCell>
-                    <TableCell className="text-right">{item.booksPublished}</TableCell>
-                    <TableCell className="text-right">{item.bestSeller}</TableCell>
-                    <TableCell className="text-right">{item.averageRating}</TableCell>
-                    <TableCell className="text-right">{item.country}</TableCell>
-                    <TableCell className="text-right">
-                      <BadgeDelta deltaType={item.deltaType} size="xs">
-                        {item.status}
-                      </BadgeDelta>
-                    </TableCell>
-                  </TableRow>
-                ))}
-            </TableBody>
-          </Table>
-        </Card>
-      );
-    }
+    return (
+      <Card>
+        <MultiSelect
+          onValueChange={setSelectedLabels}
+          placeholder="Select Music Label..."
+          className="max-w-xs"
+        >
+          {musicLabels.map((item) => (
+            <MultiSelectItem key={item.name} value={item.name}>
+              {item.name}
+            </MultiSelectItem>
+          ))}
+        </MultiSelect>
+        <Table className="mt-6">
+          <TableHead>
+            <TableRow>
+              <TableHeaderCell>Name</TableHeaderCell>
+              <TableHeaderCell className="text-right">Albums Released</TableHeaderCell>
+              <TableHeaderCell className="text-right">Top Track</TableHeaderCell>
+              <TableHeaderCell className="text-right">Top Track Rating</TableHeaderCell>
+              <TableHeaderCell className="text-right">Country</TableHeaderCell>
+              <TableHeaderCell className="text-right">Status</TableHeaderCell>
+            </TableRow>
+          </TableHead>
+  
+          <TableBody>
+            {musicLabels
+              .filter((item) => isLabelSelected(item))
+              .map((item) => (
+                <TableRow key={item.name}>
+                  <TableCell>{item.name}</TableCell>
+                  <TableCell className="text-right">{item.albumsReleased}</TableCell>
+                  <TableCell className="text-right">{item.topTrack}</TableCell>
+                  <TableCell className="text-right">{item.topTrackRating}</TableCell>
+                  <TableCell className="text-right">{item.country}</TableCell>
+                  <TableCell className="text-right">
+                    <BadgeDelta deltaType={item.deltaType} size="xs">
+                      {item.status}
+                    </BadgeDelta>
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </Card>
+    );
+  }
