1aed9381af8c9b503350e76678b3a9b91fb4bdee
nn-dmt <nn@dmt.pub>
2023-11-14 13:43:11 -0800

    tracks frontpage ui

--- components/Tracks.tsx
+++ components/Tracks.tsx
@@ -86,14 +86,14 @@ const Tracks = () => {
       </div>
 <Card>
       <List>
-        {tracks.map(({ track_title, artwork }, i) => (
-          <ListItem key={i} className="bg-white p-2" style={{ position: 'relative', backgroundImage: `url(${encodeURI(artwork)})` }}>
+        {tracks.map((track, i) => (
+          <ListItem key={i} className="bg-white p-2" style={{ position: 'relative', backgroundImage: `url(${parents[track.parent]?.artwork})` }}>
            <div className="playButton float-left w-8 block"><img className="w-8" src="https://uxwing.com/wp-content/themes/uxwing/download/video-photography-multimedia/play-icon.png" />Play</div>
           
               <div className="inline-block px-4">
-              <Text className="font-bold text-gray-800">{track_title}</Text> <Text className="text-xs text-gray-400">Genre 1 •Genre 2 • Genre 3</Text>
+              <Text className="font-bold text-gray-800">{track.track_title}</Text> <Text className="text-xs text-gray-400">{track.track_artist}</Text>
                 <Text className="text-xs py-2 text-gray-700">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</Text>
-                <Link className="text-xs text-indigo-400" to={`/label/${track_title}`}>View Label</Link>
+                <Link to={`/releases/${parents[track.parent]?.title}/${track.track_title}/by/${track.track_artist}`}>View Release</Link>
               </div>
               
             
